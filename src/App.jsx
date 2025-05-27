8cd8459551066926a0738ba6df1874693623fa00
ptzburn <milan.hokkanen@gmail.com>
2025-05-27 21:21:43 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Header from './components/Header.jsx'
+
+const App = () => {
+  return (
+    <div className="flex flex-col items-center">
+      <Header />
+    </div>
+  )
+}
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+      <a href="/">
+        <img src="/logo.svg" alt="translated.io" width="200" />
+      </a>
+    </header>
+  )
+}
+export default Header
